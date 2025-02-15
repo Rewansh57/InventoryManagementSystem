bfed292d57e0ef14cabb56cebfb18a864a746048
Rewansh57 <rewanshchoudhary57@gmail.com>
2025-02-15 09:37:18 +0530

    Update InventoryApplication.java

--- src/main/java/com/managementsystem/Inventory/InventoryApplication.java
+++ src/main/java/com/managementsystem/Inventory/InventoryApplication.java
@@ -5,20 +5,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 /*
-Project Structure
 com.managementsystem.Inventory
-│── config
-│   └── SecurityConfig.java
-│── controller
-│   └── ItemController.java
-│── model
-│   └── Item.java
-│── repository
-│   └── ItemRepository.java
-│── service
-│   ├── ItemService.java
-│   ├── CustomUserDetailsService.java
-│── MainApplication.java
+│
+├── config
+│   └── SecurityConfig.java       // Configures Spring Security(main problem arises here for looping endpoints)
+│
+├── controller
+│   ├── AuthController.java       // Handles signup and (optional to counter nullpointer exception) login endpoints
+│   └── ItemController.java       // Handles CRUD endpoints and fuzzy search for items
+│
+├── model
+│   ├── Item.java                 // Represents an inventory item entity(quantity ,etc details)
+│   └── User.java                 // Represents a user entity (with username, email, password)
+│
+├── repository
+│   ├── ItemRepository.java       // JPA repositories(uses spring data JPA ,hibernate for persistance)
+│   └── UserRepository.java       
+│
+├── service
+│   ├── ItemService.java          // Contains business logic for items (CRUD & fuzzy search)
+│   ├── UserService.java          // Contains business logic for user registration
+│   └── CustomUserDetailsService.java  // Loads user details for Spring Security
+│
+└── DemoApplication.java          // Main Spring Boot application class(The build tool is MAVEN)
 */
 public class InventoryApplication {
 
