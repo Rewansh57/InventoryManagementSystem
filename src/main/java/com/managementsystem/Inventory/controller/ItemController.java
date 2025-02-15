1b310dbac4cec8a0213394b593c9fd9efc38ac5b
Rewansh57 <rewanshchoudhary57@gmail.com>
2025-02-15 09:45:56 +0530

    Update ItemController.java

--- src/main/java/com/managementsystem/Inventory/controller/ItemController.java
+++ src/main/java/com/managementsystem/Inventory/controller/ItemController.java
@@ -14,34 +14,39 @@ import java.util.Optional;
 public class ItemController {
     @Autowired
     private ItemService itemService;
+    //Posting an item 
     @PostMapping
     public Item createItem(@RequestBody Item item){
         return itemService.createItem(item);
 
     }
+    //Getting all items 
     @GetMapping
     public List<Item> getAllItems(){
         return itemService.getAllItems();
 
     }
+    //Get item by id avoiding nullpointer exception
     @GetMapping("/{id}")
     public ResponseEntity<Item> getItem(@PathVariable Long  id){
         Optional<Item> optionalItem=itemService.getItembyId(id);
         return optionalItem.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
 
     }
+    //updation of posted details 
     @PutMapping("/{id}")
     public Item updateItem(@PathVariable Long id ,@RequestBody Item item){
         return itemService.updateItem(id,item);
 
     }
+    // deleting given details 
     @DeleteMapping("/{id}")
     public String  deleteItem(@PathVariable Long id){
         itemService.deleteItem(id);
         return "Item deleted successfully";
 
     }
-    // ✅ Fuzzy search endpoint
+    //  Fuzzy search endpoint
     @GetMapping("/search")
     public List<Item> searchItems(@RequestParam String keyword) {
         return itemService.searchItems(keyword);
